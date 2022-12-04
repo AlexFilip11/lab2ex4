22c04d7aa46c1bd13ef6b19e5c869edce69827d4
Alex Filip <alexfilippaul@gmail.com>
2022-12-04 23:54:45 +0200

    Add project files.

---
+++ lab2ex4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab2ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Scrieti un program care va determina daca un numar este sau nu palindrom.
+                        Console.WriteLine("Introduceti un numar ");
+            int n = int.Parse(Console.ReadLine());
+            int dublura, palindrom;
+            palindrom = n % 10;
+            dublura = n / 10;
+            while(dublura!=0)
+            {
+                palindrom = (palindrom * 10) + (dublura % 10);
+                dublura = dublura / 10;
+            }
+            if (n == palindrom)
+            {
+                Console.WriteLine("Numarul introdus este un palindrom");
+            }
+            else
+            {
+                Console.WriteLine("Numarul introdus nu este un palindrom");
+            }
+        }
+    }
+}
